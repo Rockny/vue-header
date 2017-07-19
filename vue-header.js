fd9e6a090d339abc797146c6f9ac2035f8f305bd
Rockny <759231112@qq.com>
2017-07-19 10:14:38 +0800

    Add files via upload

---
+++ vue-header.js
@@ -0,0 +1,40 @@
+module.exports = function (Vue) {
+  var setTitle = function (title, img) {
+    if (title === undefined || window.document.title === title) {
+      return
+    }
+    document.title = title
+  }
+  Vue.directive('header-title', function (el, binding) {
+    setTitle(binding.value, el.getAttribute('img-set') || null)
+  })
+  var setkeyWord = function (title) {
+    if (title === undefined || window.document.title === title) {
+      return
+    }
+    var metas = document.getElementsByTagName('meta')
+    for (var key in metas) {
+      if (key.toLowerCase() === 'keywords'.toLowerCase()) {
+        metas[key].content = title
+      }
+    }
+  }
+  Vue.directive('header-keywords', function (el, binding) {
+    setkeyWord(binding.value)
+  })
+  var setdescription = function (title) {
+    if (title === undefined || window.document.title === title) {
+      return
+    }
+    var metas = document.getElementsByTagName('meta')
+    for (var key in metas) {
+      if (key.toLowerCase() === 'description'.toLowerCase()) {
+        metas[key].content = title
+      }
+    }
+  }
+  Vue.directive('header-description', function (el, binding) {
+    setdescription(binding.value)
+  })
+}
+
